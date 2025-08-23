6ecd71bde648cbfcb9d41764b922385e675a6e29
ganeshRT-dev <gkumili@randomtrees.com>
2025-08-23 21:43:54 +0530

    Updated the code

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 import { HeroScrollDemo } from "./ProjectImageScroll";
